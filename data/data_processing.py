include electric field, computation of zstar and fixed bugs in ase interface

data/data_processing.py
@@ -48,7 +48,7 @@ def get_energy_json(file, species):
         energy *= (Ry2eV * 2)
     return [nspec, in_spec, energy]
 
-def get_energy_ase(atom, species):
+def get_energy_ase(atom, species,energy_key):
     Ry2eV = 13.6057039763
     symbols = list(atom.symbols)
 
@@ -60,7 +60,10 @@ def get_energy_ase(atom, species):
             in_spec.append(sp)
 
         nspec.append(Nsp)
-    energy = atom.get_potential_energy()
+    try:
+        energy = atom.get_potential_energy()
+    except:
+        energy = atom.info[energy_key]
 #    print(energy)
     return [nspec, in_spec, energy]
 
@@ -169,7 +172,7 @@ def input_function(x, shuffle=True, batch_size=32): # inner function that will b
 #    dataset = dataset.batch(batch_size).repeat(num_epochs) # split dataset into batch_size batches and repeat process for num_epochs
 #    return dataset
 
-def process_ase(atoms, evaluate_test,species,atomic_energy,C6_spec):
+def process_ase(atoms, evaluate_test,species,atomic_energy,C6_spec,energy_key,force_key):
         # Ensure a nonzero box
         if atoms.cell is None or np.linalg.norm(atoms.cell) < 1e-6:
             atoms.set_cell(np.eye(3) * 100)
@@ -181,8 +184,12 @@ def process_ase(atoms, evaluate_test,species,atomic_energy,C6_spec):
         )
 
         if evaluate_test >= 0:
-            energy = atoms.get_potential_energy() - E0
-            forces = atoms.get_forces()
+            try:
+                energy = atoms.get_potential_energy() - E0
+                forces = atoms.get_forces()
+            except:
+                energy = atoms.info[energy_key]
+                forces = atoms.get_array(force_key)
         else:
             energy = 0.0
             forces = np.zeros((len(atoms.positions), 3))
@@ -210,7 +217,7 @@ def _process_file(args):
         forces = atoms.get_array(force_key)
     else:
         atoms = file
-        atoms, energy, forces = process_ase(atoms, evaluate_test, species,atomic_energy,C6_spec)
+        atoms, energy, forces = process_ase(atoms, evaluate_test, species,atomic_energy,C6_spec, energy_key, force_key)
 
     gaussian_width = np.array([covalent_radii[x] for x in atoms.get_chemical_symbols()])
     
@@ -243,11 +250,21 @@ def load_structure_data_parallel(files, data_format, species, atomic_energy,
     Parallel version of load_structure_data.
     """
     covalent_radii = {x:element(x).covalent_radius*0.01 for x in species}
+    if evaluate_test < 0: # this is inference basically with ASE
+
+        args = (files[0], data_format, species, atomic_energy, C6_spec, energy_key, force_key, rc, evaluate_test,covalent_radii)
+        data = _process_file(args)
+        _data = {k:[] for k in data.keys()}
+        for k, v in data.items():
+            _data[k].append(v)
+        return _data
+
+    #print(files, data_format, species, atomic_energy, C6_spec, energy_key, force_key, rc, evaluate_test,covalent_radii)
     args_iter = (
         (f, data_format, species, atomic_energy, C6_spec, energy_key, force_key, rc, evaluate_test,covalent_radii)
         for f in files
     )
-
+    #print(_process_file(args_iter[0]))
     results = []
     with ProcessPoolExecutor(max_workers=max_workers) as exe:
         futures = [exe.submit(_process_file, args) for args in args_iter]
@@ -400,7 +417,11 @@ def prepare_and_split_atoms(files, species, data_format,
             to_eV = 27.211324570273 * 0.529177**6
             C6 = np.asarray([C6_spec[ss] for ss in symbols])
             atoms.new_array('C6', C6)
-            atoms.info = {'energy':atoms.get_potential_energy()-E0}
+            try:
+                energy = atoms.get_potential_energy()
+            except:
+                energy = atoms.info[energy_key]
+            atoms.info = {'energy':energy-E0}
 
         all_atoms.append(atoms)
 
@@ -457,7 +478,7 @@ def data_preparation(data_dir, species, data_format,
             if data_format == 'panna_json':
                 Nspec, spec, ene = get_energy_json(file,species)
             else:
-                Nspec, spec, ene =  get_energy_ase(file,species)
+                Nspec, spec, ene =  get_energy_ase(file,species,energy_key)
 
           
             mat_A.append(Nspec)

data/tfr_data_processing.py
@@ -98,7 +98,7 @@ def load_tfrs(filenames,batch_size):
     ignore_order.experimental_deterministic = False  # disable order, increase speed
     dataset = tf.data.TFRecordDataset(
         filenames,
-#        buffer_size=batch_size,
+        #buffer_size=,
         num_parallel_reads=AUTOTUNE
     )  # automatically interleaves reads from multiple files
     dataset = dataset.with_options(
@@ -111,19 +111,16 @@ def load_tfrs(filenames,batch_size):
     #                      num_parallel_calls=AUTOTUNE)
 
     # returns a dataset
-    dataset.cache()
+    #dataset.cache()
     return dataset
 
 def get_tfrs(filenames, batch_size,repeat_count=None):
     AUTOTUNE = tf.data.AUTOTUNE
     dataset = load_tfrs(filenames,batch_size)
     dataset = dataset.shuffle(buffer_size=1024)
-    dataset = dataset.prefetch(buffer_size=AUTOTUNE)
     dataset = dataset.batch(batch_size,
                             num_parallel_calls=AUTOTUNE,
                             deterministic=False)
-    #if repeat_count is None:
-    #    repeat_count = batch_size
-    #dataset = dataset.repeat(count=repeat_count)
+    dataset = dataset.prefetch(buffer_size=AUTOTUNE)
     return dataset
 

functions/ewald.py
@@ -4,14 +4,17 @@ import scipy.constants as constants
 import math
 #pi = tf.constant(math.pi)
 pi = 3.141592653589793
-CONV_FACT = 1e10 * constants.e / (4 * pi * constants.epsilon_0)
+
+constant_e = 1.602176634e-19
+constant_eps = 8.8541878128e-12
+CONV_FACT = 1e10 * constant_e / (4 * pi * constant_eps)
 
 class ewald:
     '''
     A pyth on class to implement ewasld sum with gaussian charge distribution
     '''
     def __init__(self, positions, cell, n_atoms, gaussian_width,
-                 accuracy, gmax,pbc): 
+                 accuracy, gmax,pbc=False,efield=None): 
         '''
            positions: atomic positions in Angstrom
            cell: cell dimension
@@ -35,8 +38,13 @@ class ewald:
         if self.pbc:
             self.reciprocal_cell = 2 * pi * tf.transpose(tf.linalg.inv(cell))
             gamma_max = 1/(tf.sqrt(2.0)*tf.reduce_min(gaussian_width))
-            self._gmax = gmax if gmax else 2.* gamma_max * tf.sqrt(-tf.log(accuracy))
-        
+            self._gmax = gmax if gmax else 2.* gamma_max * tf.sqrt(-tf.math.log(accuracy))
+        if efield is not None:
+            self.efield = tf.squeeze(efield)
+            self.reciprocal_cell = 2 * pi * tf.transpose(tf.linalg.inv(cell))
+        else:
+            self.efield = [0.,0.,0.]
+
     def real_space_term(self):
         '''
         calculates the self interaction contribution to the electrostatic energy
@@ -108,9 +116,12 @@ class ewald:
 
         # Build integer index grid for k-vectors
         # Estimate index ranges for each dimension
-        b1_norm, b2_norm, b3_norm = tf.linalg.norm(self.reciprocal_cell, axis=-1)
+        b_norm = tf.linalg.norm(self.reciprocal_cell, axis=-1)
+        b1_norm = b_norm[0]
+        b2_norm = b_norm[1]
+        b3_norm = b_norm[2]
         nmax1 = tf.cast(tf.math.floor(self._gmax / b1_norm), tf.int32)
-        nmax2 = tf.cast(tf.math.floor(self._gmax/ b2_norm), tf.int32)
+        nmax2 = tf.cast(tf.math.floor(self._gmax / b2_norm), tf.int32)
         nmax3 = tf.cast(tf.math.floor(self._gmax / b3_norm), tf.int32)
         
         n1 = tf.range(-nmax1, nmax1 + 1, dtype=tf.int32)
@@ -129,7 +140,9 @@ class ewald:
         nonzero = ~tf.reduce_all(tf.equal(replicas, 0), axis=1)
         mask = tf.logical_and(g_norm <= self._gmax, nonzero)
         g_vecs = tf.boolean_mask(g_all, mask)  # [K,3]
+        g_norm = tf.boolean_mask(g_norm, mask) #[K,
         g_sq = g_norm*g_norm  # [K]
+        
 
         # Prepare factors for summation
         # exp_factor[k,i] = exp(-g^2 * gamma_ij**2/2)
@@ -146,6 +159,15 @@ class ewald:
         Vij = tf.reduce_sum(exp_ij_by_g_sq * cos_term, axis=-1)  # [N,N]
 
         # Multiply by prefactor (charges, 4π/V, Coulomb constant e²/4πε0 in eV·Å units) so that E = 1/2 \sum_ij Vij
-        Vij *= CONV_FACT * (4.* np.pi / volume)
+        Vij *= CONV_FACT * (4.* np.pi / self.volume)
         
         return Vij 
+    def sawtooth_PE(self):
+        #k = n1 b1 + n2 b2 + n3 b3. We will set n1 = n2 = n3
+        g = tf.reduce_sum(self.reciprocal_cell, axis=0)
+        positions = self._positions
+        gr = tf.math.sin(tf.einsum('ij,j->ij', positions, g))
+        kernel = tf.einsum('ij,j,j->i', gr, self.efield, 1.0 / (g + 1e-12))
+        return kernel
+
+        

silssf/ase_interface.py
@@ -2,8 +2,8 @@ import numpy as np
 
 from ase.calculators.calculator import FileIOCalculator,Calculator, all_changes
 from ase.io import write
-#import tensorflow as tf
-#import tensorflow as tf
+from data.unpack_tfr_data import unpack_data
+import tensorflow as tf
 from models.model import mBP_model
 from data.data_processing import data_preparation, atomic_number
 
@@ -17,7 +17,7 @@ from pathlib import Path
 import os
 
 class wBP_Calculator(Calculator):
-    implemented_properties = ['energy', 'forces', 'stress']
+    implemented_properties = ['energy', 'forces', 'stress','charges']
     #implemented_properties = ['energy', 'forces']
 #    ignored_changes = {'pbc'}
 #    discard_results_on_any_change = True
@@ -26,7 +26,7 @@ class wBP_Calculator(Calculator):
 #        stdout_name='weighted_mBP.out')
 
     def __init__(self, 
-                 config=None,
+                 config=None,efield=None,
                  **kwargs):
         """Construct weighted Behler Parrinello  calculator.
 
@@ -39,6 +39,7 @@ class wBP_Calculator(Calculator):
         
         #Read in model parameters
         #I am parsing yaml files with all the parameters
+
         #
         with open(config) as f:
             configs = yaml.safe_load(f)
@@ -48,7 +49,11 @@ class wBP_Calculator(Calculator):
             if key not in configs.keys():
                 configs[key] = _configs[key]
         
-        #
+        if efield is not None:
+            print('applying electric field of: ',efield)
+            self.efield = efield
+            configs['efield'] = efield
+
         if configs['include_vdw']:
             rc = np.max([configs['rc_rad'],configs['rmax_d']])
         else:
@@ -58,9 +63,10 @@ class wBP_Calculator(Calculator):
         atomic_energy = configs['atomic_energy']
         if len(atomic_energy)==0:
             with open (os.path.join(configs['model_outdir'],'atomic_energy.json')) as df:
-                atomic_energy = json.load(df)
+                self.atomic_energy_dic = json.load(df)
 
-        self.atomic_energy = np.array([atomic_energy[key] for key in configs['species']])
+        print(atomic_energy)
+        self.atomic_energy = np.array([self.atomic_energy_dic[key] for key in configs['species']])
         self.species_identity = np.array([atomic_number(key) for key in configs['species']])
         configs['batch_size'] = 1
         model_outdir = configs['model_outdir']
@@ -70,15 +76,31 @@ class wBP_Calculator(Calculator):
     #    ckpts = [os.path.join(model_outdir+"/models", x.split('.weights.h5')[0])
     #         for x in os.listdir(model_outdir+"/models") if x.endswith('h5')]
         #ckpts.sort()
-        ckpts_idx = [int(ck.split('-')[-1]) for ck in ckpts]
+        ckpts_idx = [int(ck.split('-')[-1].split('.')[0]) for ck in ckpts]
+        #ckpts_idx = [int(ck.split('-')[-1]) for ck in ckpts]
         ckpts_idx.sort()
         epoch = ckpts_idx[-1]
         idx=f"{epoch:04d}"
-        ck = [model_outdir+"/models/"+f"ckpts-{idx}.ckpt"]
-        #self.ckpt = model_outdir+"/models/"+f"ckpts-{idx}.weights.h5"
-        
+        self.ckpt = model_outdir+"/models/"+f"ckpts-{idx}.ckpt"
+        print(f'##################################################################')
+        print(f'calculation are performed with the model:')
+        print(f'{self.ckpt}')
+        print(f'##################################################################')
 
-        self.model.load_weights(ckpts[-1]).expect_partial()
+
+        #self.ckpt = model_outdir+"/models/"+f"ckpts-{idx}.weights.h5"
+        species_identity = [atomic_number(s) for s in configs['species']]
+        #print(species_identity)
+                
+        #model_call = mBP_model
+        configs['species_identity'] = species_identity
+#        print(species_identity, len(species_identity))
+        self.model = mBP_model(configs)
+        self.model.load_weights(self.ckpt).expect_partial()
+        #weights = self.model.get_weights()
+        #print(weights[0])
+
+        #self.model.load_weights(ckpts[-1]).expect_partial()
         self.configs = configs
     def calculate(self, atoms=None, properties=['energy'], system_changes=all_changes):
         Calculator.calculate(self, atoms)
@@ -95,31 +117,37 @@ class wBP_Calculator(Calculator):
                                  1,
                                  test_fraction=0,
                                  atomic_energy=self.atomic_energy,
+                                 atomic_energy_file=os.path.join(configs['model_outdir'],'atomic_energy.json'),
                                  model_version=configs['model_version'],
                                  model_dir='./',
                                  evaluate_test=-1,
                                  max_workers=1)
-        print(self.ckpt)
+        #print(self.ckpt)
+        
+        inputs = unpack_data(list(data)[0])
+        outs = self.model(inputs)
+        e0 = np.sum([self.atomic_energy_dic[s] for s in atoms.get_chemical_symbols()])
+        energy = outs['energy'].numpy()[0] + e0
+        forces = tf.squeeze(outs['forces']).numpy()
+        stress = tf.squeeze(outs['stress']).numpy()
+        charges = tf.squeeze(outs['charges']).numpy()
+        zstar = tf.squeeze(outs['Zstar']).numpy()
 
+        '''
         configs['species_identity'] = species_identity
-#        print(species_identity, len(species_identity))
-        model = self.model_call(configs)
-        model.load_weights(self.ckpt, skip_mismatch=True)
-        weights = model.get_weights()
-
-        print(weights[0])
-#        print(data)
-#        print(model(data))
         if configs['coulumb']:
-            e_ref, e_pred, metrics, force_ref, force_pred,nat,charges,stress = model.predict(data)
+            e_ref, e_pred, metrics, force_ref, force_pred,nat,charges,stress = self.model.predict(data)
         else:
-            e_ref, e_pred, metrics, force_ref, force_pred,nat,stress = model.predict(data)
+            e_ref, e_pred, metrics, force_ref, force_pred,nat,stress = self.model.predict(data)
             charges = np.zeros_like(atoms.get_chemical_symbols())
+        '''
 
-        #print(e_pred, force_pred)
+#        print(e_pred, force_pred)
         #there is the batch axis
         self.results = {
-            "energy":e_pred[0],
-            "forces":force_pred[0],
-            "stress":stress[0]} 
+            "energy":energy,
+            "forces":forces,
+            "stress":stress,
+            "charges":charges,
+            "zstar":zstar} 
 

silssf/train.py
@@ -72,7 +72,9 @@ def default_config():
         'accuracy': 1e-6,
         'total_charge': 0.0,
         'features': False,
-        'normalize': False
+        'normalize': False,
+        'efield': None,
+        'is_training': True
     }
 def get_compiled_model(configs,optimizer,example_input):
     
@@ -80,13 +82,13 @@ def get_compiled_model(configs,optimizer,example_input):
     #We should do something for tensorflow > 2.15.0
     #fake call to build the model
     # This will “dry run” through call() and allocate weights:
-    example_input = unpack_data(example_input)
-    outs = _model(example_input, training=False)
+    #example_input = unpack_data(example_input)
+    #outs = _model(example_input, training=False)
     _model.compile(optimizer=optimizer,
                   loss=help_fn.quad_loss,
                   loss_f = help_fn.force_loss)
 
-    print(_model.summary())
+#    print(_model.summary())
     return _model
 
 class CustomCallback(tf.keras.callbacks.Callback):
@@ -225,8 +227,11 @@ def create_model(configs):
 
     if os.path.exists(model_outdir+"/tmp_backup"):
         print(f"Restoring from checkpoint {model_outdir}+'/tmp_backup'")
-        epochs = np.loadtxt(model_outdir+"/metrics.dat",skiprows=1, usecols=0)
-        last_epoch = int(epochs[-1])
+        epochs = np.loadtxt(model_outdir+"/metrics.dat",skiprows=1, usecols=0).tolist()
+        if type(epochs) is list: 
+            last_epoch = int(epochs[-1])
+        else:
+            last_epoch = int(epochs)
     else:
         last_epoch = 0
 
@@ -267,6 +272,7 @@ def create_model(configs):
     
     #checkpoint_path = model_outdir+"/models/ckpts-{epoch:04d}-{val_loss:.5f}.keras"
 #    checkpoint_path = model_outdir+"/models/ckpts-{epoch:04d}.keras"
+    '''
     if tf.config.list_physical_devices('GPU'):
         strategy = tf.distribute.MirroredStrategy()
         print(f'found {strategy.num_replicas_in_sync} GPUs!')
@@ -275,7 +281,8 @@ def create_model(configs):
     
     global_batch_size = (configs['batch_size'] *
                      strategy.num_replicas_in_sync)
-    #global_batch_size = configs['batch_size']
+    '''
+    global_batch_size = configs['batch_size']
     # Data preparation
     print('Preparing data...')
     if configs['include_vdw']:
@@ -316,8 +323,8 @@ def create_model(configs):
                                                           delete_checkpoint=False)
     #    callbacks.append(backupandrestore)
 
-    with strategy.scope():
-        model = get_compiled_model(configs,
+    #with strategy.scope():
+    model = get_compiled_model(configs,
                                    opt(configs, lr_schedule),list(train_data)[0])
     model.save_weights(checkpoint_path.format(epoch=0))
     #model.save(checkpoint_path.format(epoch=0))